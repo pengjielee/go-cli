9beca2995a09321bde7d530639feddeb58ac5d81
pengjielee <386276251@qq.com>
2023-03-03 11:49:52 +0800

    feat: show date

--- main.go
+++ main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
-const VERSION = "1.0.0"
+const VERSION = "1.0.1"
 
 func main() {
 	var showVersion bool
+	var showDate bool
 	flag.BoolVar(&showVersion, "v", false, "查看版本")
+	flag.BoolVar(&showDate, "d", false, "查看日期")
 
 	flag.Parse()
 
 	if showVersion {
 		fmt.Println(VERSION)
+	}
+
+	if showDate {
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05")) //2006-01-02 15:04:05据说是golang的诞生时间，固定写法
 	}
 }
